e1706bea11f6f538493302e00d7d0469b4a0bf57
Mateus Luiz <mateuslfreitasb@gmail.com>
2021-11-29 00:10:01 -0300

    Remove duplicated method

--- app/queries/query_dns_records_by_excluded_host_list.rb
+++ app/queries/query_dns_records_by_excluded_host_list.rb
@@ -1,21 +1,17 @@
 class QueryDnsRecordsByExcludedHostList
-  def self.call(dns_records, excluded_hosts)
-    return dns_records if excluded_hosts.blank?
+  def self.call(dns_records, excluded_hosts_list)
+    return dns_records if excluded_hosts_list.blank?
 
-    records_to_remove = records_to_remove(dns_records, excluded_hosts)
+    records_to_remove = records_to_remove(dns_records, excluded_hosts_list)
 
     dns_records
       .where
       .not(id: records_to_remove.pluck(:id))
   end
 
-  def self.records_to_remove(dns_records, excluded_hosts)
+  def self.records_to_remove(dns_records, excluded_hosts_list)
     dns_records
       .joins(:hostnames)
-      .where(hostnames: { name: hostnames_list(excluded_hosts) })
-  end
-
-  def self.hostnames_list(excluded_hosts)
-    excluded_hosts.include?(',') ? excluded_hosts.split(',') : [excluded_hosts]
+      .where(hostnames: { name: excluded_hosts_list })
   end
 end
